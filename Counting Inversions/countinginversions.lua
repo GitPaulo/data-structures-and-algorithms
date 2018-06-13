b2ae3fa8b17ce5ab29bb48aca35a9f156774dc5f
paulo_local <paulo@100.75.21.147>
2018-06-13 02:26:06 +0100

    Fix indentation

--- Counting Inversions/countinginversions.lua
+++ Counting Inversions/countinginversions.lua
@@ -1,33 +1,33 @@
 --[[
-	Counting Inversions - Simple
-		- How far the array is from being sorted. If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count is the maximum.
+    Counting Inversions - Simple
+        - How far the array is from being sorted. If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count is the maximum.
 
-	Time Complexity:
-		- O(n*n)
-	Space Complexity:
-		- O(1)
+    Time Complexity:
+        - O(n*n)
+    Space Complexity:
+        - O(1)
 ]]
 
 function CountingInversionsSimple( arr )
-	local inv_count = 0;
-  	for i=1, n, 1 do
-		for j=i+1, n, 1 do
-      		if (arr[i] > arr[j]) then
-        		inv_count++;
-        	end
+    local inv_count = 0;
+    for i=1, n, 1 do
+        for j=i+1, n, 1 do
+            if (arr[i] > arr[j]) then
+                inv_count++;
+            end 
         end
     end
-	return inv_count;
+    return inv_count;
 end
 
 --[[
-	Counting Inversions - Merge Sort
-		- How far the array is from being sorted. If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count is the maximum.
+    Counting Inversions - Merge Sort
+        - How far the array is from being sorted. If array is already sorted then inversion count is 0. If array is sorted in reverse order that inversion count is the maximum.
 
-	Time Complexity:
-		- O(nlogn), same as merge sort
-	Space Complexity:
-		- same as merge sort
+    Time Complexity:
+        - O(nlogn), same as merge sort
+    Space Complexity:
+        - same as merge sort
 ]]
 
 function merge( arr, temp, low, mid, high )
@@ -38,30 +38,30 @@ function merge( arr, temp, low, mid, high )
      
     while ((i <= mid - 1) && (j <= high)) do
         if (arr[i] <= arr[j]) then
-        	k = k + 1; i = i + 1;
-        	temp[k] = arr[i];
+            k = k + 1; i = i + 1;
+            temp[k] = arr[i];
         else
-        	k = k + 1; j = j + 1;
-        	temp[k] = arr[j];
-        	--this is tricky -- see above explanation/diagram for merge()
-          	inv_count = inv_count + (mid - i);
+            k = k + 1; j = j + 1;
+            temp[k] = arr[j];
+            --this is tricky -- see above explanation/diagram for merge()
+            inv_count = inv_count + (mid - i);
         end
     end
       
-	-- Copy the remaining elements of left subarray (if there are any) to temp
-	while (i <= mid - 1) do 
-		k = k + 1; i = i + 1;
-		temp[k] = arr[i];
-  	end
-	-- Copy the remaining elements of right subarray (if there are any) to temp
-  	while (j <= high) do
-  		k = k + 1; j = j + 1;
-    	temp[k] = arr[j];
-  	end
+    -- Copy the remaining elements of left subarray (if there are any) to temp
+    while (i <= mid - 1) do 
+        k = k + 1; i = i + 1;
+        temp[k] = arr[i];
+    end
+    -- Copy the remaining elements of right subarray (if there are any) to temp
+    while (j <= high) do
+        k = k + 1; j = j + 1;
+        temp[k] = arr[j];
+    end
 
- 	-- Copy back the merged elements to original array*/
-  	for i=low, high, 1 do
-    	arr[i] = temp[i];
+    -- Copy back the merged elements to original array*/
+    for i=low, high, 1 do
+        arr[i] = temp[i];
     end
 
     return inv_count;
